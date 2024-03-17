425cc253857d18fcd718715376e0d413266f8a3d
Mark Hartnady <hartnady@gmail.com>
2024-03-17 10:50:21 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Database setup
-username = "YOUR_USERNAME_HERE"
-password = "YOUR_PASSWORD_HERE"
+username = "YOUR_MYSQL_USERNAME_HERE"
+password = "YOUR_MYSQL_PASSWORD_HERE"
 hostname = f"{username}.mysql.pythonanywhere-services.com"
 databasename = f"{username}$default"
 
@@ -10,4 +10,4 @@ SQLALCHEMY_DATABASE_URI = (
 SQLALCHEMY_ENGINE_OPTIONS = {"pool_recycle": 299}
 SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
-OPEN_AI_KEY = 'your_open_ai_key_here'
+OPEN_AI_KEY = 'YOUR_OPEN_AI_KEY_HERE'
